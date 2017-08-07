a39f886467113590245232721adf0a50d9e22fd0
Vinícius Saueia <vsaueia@gmail.com>
2017-08-07 17:59:00 -0400

    fix no teste

--- src/modelo/jogadas/carta-alta.js
+++ src/modelo/jogadas/carta-alta.js
@@ -2,11 +2,11 @@ export class CartaAlta {
   static Nome = 'CartaAlta';
   static calcularRankingParaDesempate(maoDoJogador) {
     let valoresDasCartas = maoDoJogador.obterValoresDasCartasEmOrdemCrescente();
-    let fator = 100000;
+    let fator = 0.1;
     let ranking = 0;
     valoresDasCartas.forEach(valor => {
       ranking += valor / fator;
-      fator /= 10;
+      fator *= 10;
     });
     return ranking;
   }
